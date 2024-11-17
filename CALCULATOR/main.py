7b0fb064466540abfc07606edcd9da5afeeb8b9c
neivanny1 <nevileneivanny@gmail.com>
2024-11-17 11:41:11 +0000

    added modulus and bitwise ops

--- CALCULATOR/main.py
+++ CALCULATOR/main.py
@@ -4,7 +4,9 @@
 Simple Calculator with ASCII Banner and Colors
 '''
 
-# ANSI color codes
+'''
+ANSI color codes
+'''
 class Colors:
     HEADER = '\033[95m'
     BLUE = '\033[94m'
@@ -16,7 +18,9 @@ class Colors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-# ASCII banner
+'''
+ASCII banner
+'''
 def print_banner():
     print(Colors.BLUE + Colors.BOLD)
     print(r"""
@@ -28,8 +32,30 @@ def print_banner():
 
     """)
     print(Colors.ENDC)
+'''
+Bitwise handler
+Takes in 3 arguments: operation, int num1 and int num2
+'''
+def bitwiseHandler(num1, num2, operation):
+    if num1.is_integer() and num2.is_integer():
+        int1, int2 = int(num1), int(num2)
+        if operation == '&':
+            result = int1 & int2
+            print(Colors.GREEN + f"The result of {int1} & {int2} is: {result}" + Colors.ENDC)
+        elif operation == '|':
+            result = int1 | int2
+            print(Colors.GREEN + f"The result of {int1} | {int2} is: {result}" + Colors.ENDC)
+        elif operation == '^':
+            result = int1 ^ int2
+            print(Colors.GREEN + f"The result of {int1} ^ {int2} is: {result}" + Colors.ENDC)
+        else:
+            print(Colors.RED + "Error: Bitwise operations require integer inputs." + Colors.ENDC)
 
-# Calculator function
+  
+
+"""
+Calculator function
+"""
 def calculator():
     print_banner()
     print(Colors.GREEN + "Welcome to the Scientific Calculator!" + Colors.ENDC)
@@ -46,13 +72,17 @@ def calculator():
         print("8: Bitwise OR (|)")
         print("9: Bitwise XOR (^)" + Colors.ENDC)
         
-        # Get user inputs
+        '''
+        Get user inputs
+        '''
         try:
             num1 = float(input(Colors.BOLD + "\nEnter the first number: " + Colors.ENDC))
             num2 = float(input(Colors.BOLD + "Enter the second number: " + Colors.ENDC))
             operation = input(Colors.BLUE + "Enter the operation (+, -, *, /, %, **, &, |, ^): " + Colors.ENDC)
 
-            # Perform calculation based on chosen operation
+            '''
+            Performs calculation based on chosen operation
+            '''
             if operation == '+':
                 result = num1 + num2
                 print(Colors.GREEN + f"The result of {num1} + {num2} is: {result}" + Colors.ENDC)
@@ -75,20 +105,8 @@ def calculator():
                 result = num1 ** num2
                 print(Colors.GREEN + f"The result of {num1} ** {num2} is: {result}" + Colors.ENDC)
             elif operation in ['&', '|', '^']:
-                # Bitwise operations require integers
-                if num1.is_integer() and num2.is_integer():
-                    int1, int2 = int(num1), int(num2)
-                    if operation == '&':
-                        result = int1 & int2
-                        print(Colors.GREEN + f"The result of {int1} & {int2} is: {result}" + Colors.ENDC)
-                    elif operation == '|':
-                        result = int1 | int2
-                        print(Colors.GREEN + f"The result of {int1} | {int2} is: {result}" + Colors.ENDC)
-                    elif operation == '^':
-                        result = int1 ^ int2
-                        print(Colors.GREEN + f"The result of {int1} ^ {int2} is: {result}" + Colors.ENDC)
-                else:
-                    print(Colors.RED + "Error: Bitwise operations require integer inputs." + Colors.ENDC)
+                # Calls bitwise handler
+                bitwiseHandler(num1, num2, operation)
             else:
                 print(Colors.RED + "Invalid operation selected. Please choose a valid operation." + Colors.ENDC)
         except ValueError:
